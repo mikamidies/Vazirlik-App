bfeefbe82b8a449639b202b1cebaf95512e1a5ec
mikamidies <mikamidies04@gmail.com>
2023-11-17 15:37:53 +0500

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Vazirlik-App",
+    title: "Oilaviy mehmon uylari ro‘yxati",
     htmlAttrs: {
       lang: "en",
     },
@@ -14,30 +13,19 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["ant-design-vue/dist/antd.css"],
+  css: ["ant-design-vue/dist/antd.css", "@/assets/css/main.css"],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["@/plugins/antd-ui"],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
-  ],
+  modules: ["@nuxtjs/axios"],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: "/",
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 };
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="master">
+    <div class="container">
+      <p>Hello World</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
